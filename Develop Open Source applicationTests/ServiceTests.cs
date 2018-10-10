3f5b943b84a0a604b7d3f67d7218cdf75e550c02
matthewallen7 <36918158+matthewallen7@users.noreply.github.com>
2018-10-10 16:05:12 +0800

    Add files via upload

---
+++ Develop Open Source applicationTests/ServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Develop_Open_Source_application;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Develop_Open_Source_application.Tests
+{
+    [TestClass()]
+    public class ServiceTests
+    {
+        [TestMethod()]
+        public void recordServiceTest()
+        {
+            //Arrange
+            Service test = new Service();
+
+            //Act
+            string no = test.recordService(10, 109000);
+            string yes = test.recordService(10, 110000);
+
+            //Assert
+            Assert.IsTrue(no == "N" && yes == "Y");
+        }
+    }
+}
---
+++ Develop Open Source applicationTests/VehicleTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Develop_Open_Source_application;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Develop_Open_Source_application.Tests
+{
+    [TestClass()]
+    public class VehicleTests
+    {
+        [TestMethod()]
+        public void printDetailsTest()
+        {
+            //Arrange
+            Vehicle test = new Vehicle();
+
+            //Act
+            string output = test.printDetails("Audi", "Spyder", 2018, "1PLS110", "11.4L/100km", "Red", "35000", 5000, 3, "5000", "N");
+
+            //Assert
+            Assert.IsTrue(output == "Manufacturer: " + "Audi" + "\r\n" + "Model: " + "Spyder" + "\r\n" +
+            "Year: " + 2018 + "\r\n" + "Regestration: " + "1PLS110" + "\r\n" +
+            "Fuel Economy: " + "11.4L/100km" + "\r\n" + "Colour: " + "Red" + "\r\n" + "Odometer: " + "35000" + "\r\n" +
+            "Distance Travelled: " + 5000 + "\r\n" + "Total Services: " + 3 + "\r\n" + "Revenue Recorded: " + "5000" + "\r\n" + "Requires a Service: " + "N" + "\r\n");
+        }
+
+        [TestMethod()]
+        public void addKilometersTest()
+        {
+            //Arrange
+            Vehicle test = new Vehicle();
+
+            //Act
+            double value = test.addKilometers(30000, 35000);
+
+            //Assert
+            Assert.IsTrue(value == 5000);
+        }
+    }
+}
